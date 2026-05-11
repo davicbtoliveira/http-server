4051bb319b08287678bdcfd82887ca77e49c633c
Davi Castello Branco Tavares de Oliveira <davicbtoliveira@gmail.com>
2026-05-10 21:38:05 -0400

    added handlers to routes

--- main.go
+++ main.go
@@ -12,6 +12,15 @@ func main() {
 		Addr:    ":8080",
 	}
 
+	httpMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	})
+
+	fileServer := http.FileServer(http.Dir("./app"))
+	httpMux.Handle("/app/", http.StripPrefix("/app", fileServer))
+
 	if err := httpServer.ListenAndServe(); err != nil {
 		log.Printf("error occured while serving the server: %v", err)
 		return
